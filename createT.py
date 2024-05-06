bc633afc8327cf08e6b884ea67d17519ef892ac0
Tangumaj <165401057+Tangumaj@users.noreply.github.com>
2024-05-06 10:35:29 -0500

    Create createT.py

---
+++ createT.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="tangumaj",
+    user="tangumaj",
+    password="ardi363puppy")
+    
+cur = conn.cursor()
+
+sql = """DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (
+  name text,
+  artists text,
+  dailyr int,
+  movement real,
+  wmovement real,
+  country text,
+  date date,
+  pop real,
+  expl text,
+  timems real,
+  dance real,
+  energy real,
+  acousticness real, 
+  tempo real
+);"""
+
+cur.execute( sql )
