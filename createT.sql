36cd00f7186fab54268b1b23a805c81ec3e09841
toledod <165401366+toledod@users.noreply.github.com>
2024-05-06 16:50:13 -0500

    Update createT.sql

--- createT.sql
+++ createT.sql
@@ -1,1 +1,17 @@
-wait
+DROP TABLE IF EXISTS spotify;
+CREATE TABLE spotify (
+  name text,
+  artists text,
+  dailyr int,
+  movement real,
+  wmovement real,
+  country text,
+  date date,
+  pop real,
+  expl text,
+  timems real,
+  dance real,
+  energy real,
+  acousticness real, 
+  tempo real
+  );
