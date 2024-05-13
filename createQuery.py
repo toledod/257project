3da0d7564f5fb747cef9b5df02d2a2fa07d1e34e
Tangumaj <165401057+Tangumaj@users.noreply.github.com>
2024-05-13 10:34:53 -0500

    Update createQuery.py

--- createQuery.py
+++ createQuery.py
@@ -3,9 +3,9 @@ import psycopg2
 conn = psycopg2.connect(
     host="localhost",
     port=5432,
-    database="tangumaj",
-    user="tangumaj",
-    password="ardi363puppy")
+    database="toledod",
+    user="toledod",
+    password="mask777glass")
     
 cur = conn.cursor()
 
