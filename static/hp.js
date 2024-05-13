2ddcb09a5023dd472d7dea4e14e3facda32715e7
Tangumaj <165401057+Tangumaj@users.noreply.github.com>
2024-05-13 10:40:47 -0500

    Update hp.js

--- static/hp.js
+++ static/hp.js
@@ -1,1 +1,14 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+function setRandomColor() {
+  the_heading = document.getElementById("hello");
+  the_heading.style.color = getRandomColor()
+  console.log("I just changed the color to: " + the_heading.style.color)  
+}
