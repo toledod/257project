43eaf6d4354848516372a1b04b73d656a0606993
toledod <165401366+toledod@users.noreply.github.com>
2024-05-20 19:12:03 -0500

    Update hp.js

--- static/hp.js
+++ static/hp.js
@@ -1,18 +1,3 @@
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function setRandomColor() {
-  the_heading = document.getElementById("hello");
-  the_heading.style.color = getRandomColor()
-  console.log("I just changed the color to: " + the_heading.style.color)  
-}
-
 function updateDays() {
   month = document.getElementById('month').value;
   daySelect = document.getElementById('day');
