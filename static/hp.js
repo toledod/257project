d9d262229d5a52e60f69f8d1eb7e45736b14a7e2
toledod <165401366+toledod@users.noreply.github.com>
2024-05-20 19:55:27 -0500

    Update hp.js

--- static/hp.js
+++ static/hp.js
@@ -1,12 +1,1 @@
-function updateDays2() {
-  month = document.getElementById('month').value;
-  daySelect = document.getElementById('day');
-  daysInMonth = new Date(2024, month, 0).getDate(); 
-  
-  let options = '';
-  for (let day = 1; day <= daysInMonth; day++) {
-    options += `<option value="${day}">${day}</option>`;
-  }
-  daySelect.innerHTML = options;
-  document.addEventListener('DOMContentLoaded', updateDays);
-}
+
