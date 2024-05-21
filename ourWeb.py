e13a410fc7806418d2cc4694836d0f978aa23a3b
Tangumaj <165401057+Tangumaj@users.noreply.github.com>
2024-05-20 20:00:54 -0500

    Update ourWeb.py

--- ourWeb.py
+++ ourWeb.py
@@ -23,12 +23,12 @@ def welcome():
         sql = "SELECT * FROM spotify WHERE dance = %s"
         danceInt = random.random()
         cur.execute(sql, [danceInt])
-        result1 = cur.fetchone()
+        result = cur.fetchone()
     else:
         sql = "SELECT * FROM spotify WHERE energy = %s"
         energyInt = random.random()
         cur.execute(sql, [energyInt])
-        result1 = cur.fetchone()
+        result = cur.fetchone()
     return render_template("homepage.html", someText = result[0])
 
 @app.route('/month')
