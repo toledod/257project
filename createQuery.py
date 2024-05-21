fba5bc84931443178496b061def0b6c3ebdae1c3
toledod <165401366+toledod@users.noreply.github.com>
2024-05-20 20:38:33 -0500

    Update createQuery.py

--- createQuery.py
+++ createQuery.py
@@ -7,33 +7,33 @@ conn = psycopg2.connect(
     user="toledod",
     password="mask777glass")
     
-cur = conn.cursor()
+# cur = conn.cursor()
 
-sql = "SELECT * FROM spotify WHERE country='US'"
+# sql = "SELECT * FROM spotify WHERE country='US'"
     
-cur.execute( sql )
+# cur.execute( sql )
 
-row = cur.fetchone()
+# row = cur.fetchone()
 
-if row == None:
-    print("Not found, please look for another city")
-else:
-    print(row)
+# if row == None:
+#     print("Not found, please look for another city")
+# else:
+#     print(row)
 
 cur2 = conn.cursor()
 
-sql2 = "SELECT * FROM spotify WHERE country='US' AND date='2024-04-30' ORDER BY dailyr DESC"
+sql2 = "SELECT * FROM spotify WHERE country='US' AND ORDER BY dance DESC"
 
 cur2.execute( sql2 )
 row2 = cur2.fetchone()
 
 print(row2)
 
-cur3 = conn.cursor()
+# cur3 = conn.cursor()
 
-sql3 = "SELECT * FROM spotify ORDER BY dance DESC"
+# sql3 = "SELECT * FROM spotify ORDER BY dance DESC"
 
-cur3.execute( sql3 )
-row3 = cur3.fetchone()
+# cur3.execute( sql3 )
+# row3 = cur3.fetchone()
 
-print(row3)
+# print(row3)
