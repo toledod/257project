16a57f62dae7b9ff4bd914be73e28596fa554254
Tangumaj <165401057+Tangumaj@users.noreply.github.com>
2024-05-27 20:47:32 -0500

    Update hp.js

--- static/hp.js
+++ static/hp.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const textContainer = document.getElementById('songNameHome');
-    const newText = 'This is the new text.';
+// document.addEventListener('DOMContentLoaded', () => {
+//     const textContainer = document.getElementById('songNameHome');
+//     const newText = 'This is the new text.';
 
-    // Change text after 3 seconds
-    setTimeout();
-        // Add the fade-out class
-    textContainer.classList.add('fade-out');
+//     // Change text after 3 seconds
+//     setTimeout();
+//         // Add the fade-out class
+//     textContainer.classList.add('fade-out');
 
-        // Wait for the transition to complete (1 second), then change the text and fade in
-    function setTimeout() {
-        textContainer.textContent = newText;
-        textContainer.classList.remove('fade-out');
-    }
-    });
+//         // Wait for the transition to complete (1 second), then change the text and fade in
+//     function setTimeout() {
+//         textContainer.textContent = newText;
+//         textContainer.classList.remove('fade-out');
+//     }
+//     });
