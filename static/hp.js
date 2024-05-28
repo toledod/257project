3ad170c34aad1df700d9a902d420a6eb05e94a5c
Tangumaj <165401057+Tangumaj@users.noreply.github.com>
2024-05-27 20:34:08 -0500

    Update hp.js

--- static/hp.js
+++ static/hp.js
@@ -3,14 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const newText = 'This is the new text.';
 
     // Change text after 3 seconds
-    setTimeout(() => {
+    setTimeout();
         // Add the fade-out class
-        textContainer.classList.add('fade-out');
+    textContainer.classList.add('fade-out');
 
         // Wait for the transition to complete (1 second), then change the text and fade in
-        setTimeout(() => {
-            textContainer.textContent = newText;
-            textContainer.classList.remove('fade-out');
-        }, 5000); // This should match the duration of the CSS transition
-    }, 5000); // Change text after 3 seconds (3000 milliseconds)
-});
+    function setTimeout() {
+        textContainer.textContent = newText;
+        textContainer.classList.remove('fade-out');
+    });
